ec98e0ecae1529fd974c980c5ce34b293b6e86fd
Sarah Ransohoff <sranso@gmail.com>
2016-10-12 13:53:07 -0400

    Move everything to one module.

--- Main.elm
+++ Main.elm
@@ -3,24 +3,32 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.App
--- import List
-import Die
+import List
+import Random
 
 
 -- MODEL
 
 
-type alias BoardModel =
-  { dieModel : Die.Model
+type alias Board =
+  { die : Die
   }
 
-initialModel : BoardModel
+initialModel : Board
 initialModel =
-  { dieModel = Die.initialModel
+  { die = initialDie
   }
 
 
-init : ( BoardModel, Cmd Msg )
+type alias Die =
+  Int
+
+initialDie : Die
+initialDie =
+    1
+
+
+init : ( Board, Cmd Msg )
 init =
   ( initialModel, Cmd.none )
 
@@ -29,46 +37,37 @@ init =
 
 
 type Msg
-  = DieMsg Die.Msg
-  | RollDie
+  = Roll
+  | OnResult Int
 
 
 -- VIEW
 
 
-view : BoardModel -> Html Msg
+view : Board -> Html Msg
 view model =
   div []
-    [ Html.App.map DieMsg (Die.view model.dieModel)
-    , button [ onClick RollDie ] [ text "Roll die" ]
+    [ text (toString model.die)
+    , button [ onClick Roll ] [ text "Roll die" ]
     ]
 
 
 -- UPDATE
 
 
-update : Msg -> BoardModel -> ( BoardModel, Cmd Msg )
+update : Msg -> Board -> ( Board, Cmd Msg )
 update msg model =
   case msg of
-    DieMsg subMsg ->
-      let
-          ( updatedDieModel, dieCmd ) =
-            Die.update subMsg model.dieModel
-      in
-          ( { model | dieModel = updatedDieModel }
-          , Cmd.map DieMsg dieCmd )
-    RollDie ->
-      let
-          ( updatedDieModel, dieCmd ) =
-            Die.update Die.Roll model.dieModel
-      in
-          ( { model | dieModel = updatedDieModel }
-          , Cmd.map DieMsg dieCmd )
+    Roll ->
+      ( model, Random.generate OnResult ( Random.int 1 6 ) )
+    OnResult res ->
+      ( { model | die = res }, Cmd.none )
+
 
 -- SUBSCRIPTIONS
 
 
-subscriptions : BoardModel -> Sub Msg
+subscriptions : Board -> Sub Msg
 subscriptions model =
   Sub.none
 
